b21a53166c48650a3b89a2394192acc85b81cf76
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2026-01-02 21:24:09 -0500

    gluvalcorp commit1

--- dist/database/connection.js
+++ dist/database/connection.js
@@ -22,11 +22,14 @@ var dbSettings = {
    password: config.dbPassword,
    server: config.dbServer,
    database: config.dbDatabase, */
-  user: "sax",
-  password: "Solomon2011x",
-  server: "181.198.20.179x",
+  //user: "sax",
+  user: "server",
+  password: "Solomon2011",
+  //server: "181.198.20.179x",
+  server: "seimalsa.cwh4s6o4w0t4.us-east-1.rds.amazonaws.com",
   //server: "192.168.15.6",
-  database: "DB_PINOYCAZA_x",
+  //database: "DB_PINOYCAZA_x",
+  database: "PRODUCCION",
   options: {
     encrypt: false,
     // for azure
